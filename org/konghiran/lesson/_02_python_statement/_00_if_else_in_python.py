5178f6fdd4128a269cc6e55d0c3dddfbb5397cee
the last <zealotkid711@gmail.com>
2019-12-15 21:55:08 +0700

    add if else statement

---
+++ org/konghiran/lesson/_02_python_statement/_00_if_else_in_python.py
@@ -0,0 +1,12 @@
+# if else is python control flow
+# if my dog is hungry then ...
+
+is_my_dog_hungry = False
+is_give_food = False
+
+if(is_my_dog_hungry):
+    is_give_food = True
+else:
+    is_give_food = False
+
+print('Give a food to the dog : ', is_give_food)
